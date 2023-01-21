6ccbafa5692c9b7975e7eeb528056c4f2cb50c86
Sayem <sayemahamed183@gmail.com>
2023-01-21 21:30:14 +0600

    ok

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 // Start Generic Node Class
 template <typename T>
@@ -6,7 +6,7 @@ class Node
 {
 public:
     T data;
-    Node *BiggerNodePoint,*SmallerNodePoint;
+    Node *BiggerNodePoint, *SmallerNodePoint;
     Node(){};
     Node(T data)
     {
@@ -16,7 +16,28 @@ public:
     }
 };
 // End of Generic Node Class
+
+// Start of Generic Binary Search Tree Class Implementation
+template <typename T>
+class GenericBinarySearchTree
+{
+public:
+    Node<T> *Root = NULL;
+    long long rightWeight = 0, leftWeight = 0, Size = 0;
+    void add(T data)
+    {
+        if (Root == NULL)
+        {
+            root = new Node<T>(data);
+        }
+        else
+        {
+            Node<T> *temp = Root;
+            Node<T> *New = new Node<T>(data);
+        }
+    }
+};
+// End of Generic Binary Search Tree Class Implementation
 int main()
 {
-
 }
